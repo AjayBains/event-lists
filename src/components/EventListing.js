12464baf3d42a10c1fc463769618345f430a9a39
Ajay <ajaybains2004@gmail.com>
2021-08-01 09:04:04 +0530

    add grid and list view toggle button

--- src/components/EventListing.js
+++ src/components/EventListing.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Grid, Card } from "@material-ui/core";
+import React, { useState } from "react";
+import { Container, Grid, Card, Button } from "@material-ui/core";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -11,13 +11,64 @@ const useStyles = makeStyles({});
 
 const EventListing = ({ eventList }) => {
   console.log(eventList);
+  const [isList, setIsList] = useState(true);
 
   return (
     <Container>
+      <Button
+        onClick={() => setIsList(!isList)}
+        color="primary"
+        variant="contained"
+        style={{
+          marginBottom: "4rem",
+          marginTop: "1rem",
+        }}
+      >
+        Gridview/ListView
+      </Button>
       <Grid container spacing={3}>
         {eventList.item.map((event) => {
           const { banner_url, eventname, location, label, event_id } = event;
-          return (
+          return isList ? (
+            <Grid item key={event_id} xs={12}>
+              <Card className="card__img">
+                <CardActionArea>
+                  <CardMedia
+                    alt={eventname}
+                    height="140"
+                    image={banner_url}
+                    title={eventname}
+                  />
+
+                  <CardContent>
+                    {/* <img src={banner_url} alt="pic" style={{ width: "100%" }} /> */}
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="h2"
+                      textAlign="center"
+                    >
+                      {eventname}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="h6"
+                    >
+                      {location}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="h6"
+                    >
+                      {label}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ) : (
             <Grid item key={event_id} xs={12} md={6}>
               <Card className="card__img">
                 <CardActionArea>
