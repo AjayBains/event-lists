e733d7a7f91b93fc8e4c9fc2de244afaa9636e14
Ajay <ajaybains2004@gmail.com>
2021-08-04 10:54:26 +0530

    cleaned up code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { Route, Redirect, useLocation } from "react-router-dom";
 import Listing from "./Listing";
 import Menu from "./Menu";
 
--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Container, Grid, Card, Button, Divider } from "@material-ui/core";
+import { Container, Grid, Card, Button } from "@material-ui/core";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
